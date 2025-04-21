e006b8b8f76b96a5ebbb8b8562f58e2345871605
Wes Gray <wesgray@gmail.com>
2025-04-21 00:27:50 -0700

    - Add const for Mode - Use real values for decode Read - Use real values for decode Write

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,37 @@
 use log::{debug, error, info, warn};
+
+/// Message has the following binary format
+/// 
+/// # Read/Write
+///  ```
+///  2 bytes     string    1 byte     string   1 byte
+///  ------------------------------------------------
+/// | Opcode |  Filename  |   0  |    Mode    |   0  |
+///  ------------------------------------------------
+///  ```
+///  # Data
+///  Data receives 512 Bytes in the data field each time.
+///  If < 512 Bytes are received, then this signals EOF.
+///  ```
+///  2 bytes     2 bytes      n bytes
+///  ----------------------------------
+/// | Opcode |   Block #  |   Data     |
+///  ----------------------------------
+///  ```
+///  # Ack
+///  ```
+///  2 bytes     2 bytes
+///  ---------------------
+/// | Opcode |   Block #  |
+///  ---------------------
+///  ```
+///  # Error
+///  ```
+///  2 bytes     2 bytes      string    1 byte
+///  -----------------------------------------
+/// | Opcode |  ErrorCode |   ErrMsg   |   0  |
+///  -----------------------------------------
+///  ```
 #[derive(Debug, PartialEq)]
 pub enum Message {
     Read {
@@ -35,19 +68,51 @@ pub enum State {
 }
 
 impl Message {
+    
+    const READ: u16 = 0;
+    const WRITE: u16 = 1;
+    const DATA: u16 = 2;
+    const ACK: u16 = 3;
+    const ERROR: u16 = 4;
+
+    pub fn op_code(&self) -> u16 {
+        match &self {
+            Message::Read { .. } => Self::READ,
+            Message::Write { .. } => Self::WRITE,
+            Message::Data { .. } => Self::DATA,
+            Message::Ack { .. } => Self::ACK,
+            Message::Error { .. } => Self::ERROR,
+        }
+    }
+    
     pub fn encode(&self) -> Vec<u8> {
         match self {
-            Message::Read { filename, mode } => {
+            msg @ Message::Read { filename, mode } => {
                 info!(
-                    "Message.encode: Operation:{}, filename:{}, mode: {:?}",
-                    0, filename, mode
+                    "Message.encode: Operation: Read, filename:{}, mode: {:?}",
+                    filename, mode
                 );
-                let mut c: Vec<u8> = Vec::new();
-                c.extend([0, 0]);
+                let mut c: Vec<u8> = vec![];
+                c.extend(msg.op_code().to_be_bytes());
                 c.extend(filename.clone().into_bytes());
+                c.push(0x0);
                 c.extend(mode.to_string().into_bytes());
+                c.push(0x0);
                 c
-            }
+            },
+            msg @ Message::Write { filename, mode } => {
+                info!(
+                    "Message.encode: Operation: Write, filename:{}, mode: {:?}",
+                     filename, mode
+                );
+                let mut c : Vec<u8> = vec![];
+                c.extend(msg.op_code().to_be_bytes());
+                c.extend(filename.clone().into_bytes());
+                c.push(0x0);
+                c.extend(mode.to_string().into_bytes());
+                c.push(0x0);
+                c
+            },
             _ => {
                 error!("not impl!");
                 vec![]
@@ -58,17 +123,47 @@ impl Message {
     pub fn decode(buff: &[u8]) -> Result<Message, ()> {
         debug!("{:?}", buff);
         if buff.len() < 2 {
-            warn!("len too short");
+            error!("len too short");
             return Err(());
         }
-        let op_code = &buff[0..=1];
+        let (op_code, remainder) = buff.split_at(2);
+        let op_code= u16::from_be_bytes(op_code.try_into().unwrap());
+
         match op_code {
-            [0, 0] => Ok(Message::Read {
-                filename: "foo".to_string(),
-                mode: Mode::NetAscii,
-            }),
+            Self::READ => {
+                let maybe_args: Vec<_> = remainder.split(|x| *x == 0x0)
+                    .filter(|x| !x.is_empty())
+                    .collect();
+                if maybe_args.len() != 2 {
+                    error!("Expected 2 arguments but found: {}", maybe_args.len());
+                    error!("Value: {:?}", maybe_args);
+                    return Err(())
+                }
+                let filename = String::from_utf8(maybe_args[0].to_vec()).unwrap();
+                let mode = Mode::from_bytes(maybe_args[1]).unwrap();
+                Ok(Message::Read {
+                    filename,
+                    mode,
+                })
+            },
+            Self::WRITE => {
+                let maybe_args: Vec<_> = remainder.split(|x| *x == 0x0)
+                    .filter(|x| !x.is_empty())
+                    .collect();
+                if maybe_args.len() != 2 {
+                    error!("Expected 2 arguments but found: {}", maybe_args.len());
+                    error!("Value: {:?}", maybe_args);
+                    return Err(())
+                }
+                let filename = String::from_utf8(maybe_args[0].to_vec()).unwrap();
+                let mode = Mode::from_bytes(maybe_args[1]).unwrap();
+                Ok(Message::Write {
+                    filename,
+                    mode,
+                })
+            },
             x => {
-                debug!("{:?}", x);
+                debug!("not impl, found: {:?}", x);
                 Err(())
             }
         }
@@ -90,6 +185,17 @@ pub enum Mode {
     NetAscii,
     Octet,
     Mail,
+}
+
+impl Mode {
+    fn from_bytes(bytes: &[u8]) -> Option<Self> {
+        match bytes {
+            b"netascii" => Some(Mode::NetAscii),
+            b"octet" => Some(Mode::Octet),
+            b"mail" => Some(Mode::Mail),
+            _ => None
+        }
+    }
 }
 
 impl ToString for Mode {
@@ -120,17 +226,17 @@ mod test {
             mode: Mode::NetAscii,
         };
         debug!("test: Read Message Encoding as: {:?}", read.encode());
-        assert_eq!(vec![0, 0, 102, 111, 111, 110, 101, 116, 97, 115, 99, 105, 105], read.encode());
+        assert_eq!(vec![0, 0, 102, 111, 111, 0, 110, 101, 116, 97, 115, 99, 105, 105, 0], read.encode());
     }
 
     #[test]
     fn test_decode_read() {
         // Read Message with filename: foo and mode: NetAscii
-        let read = [0, 0, 102, 111, 111];
+        let read = [0, 0, 102, 111, 111, 0, 110, 101, 116, 97, 115, 99, 105, 105, 0];
         let read_msg = Message::decode(&read);
         match read_msg {
             Ok(msg) => {
-                debug!("test: Read Message decoded: {:?}", msg);
+                debug!("Read Message decoded: {:?}", msg);
             }
             Err(e) => {
                 debug!("There was err: {:?}", e);
@@ -146,12 +252,23 @@ mod test {
     }
     #[test]
     fn test_encode_write() {
-        let write = Message::Write{filename: "foo".to_owned(), mode: Mode::NetAscii};
-        debug!("{:?}", write);
+        let obj = Message::Write{filename: "foo".to_owned(), mode: Mode::NetAscii};
+        let bytes = [0, 1, 102, 111, 111, 0, 110, 101, 116, 97, 115, 99, 105, 105, 0];
+        debug!("{:?}", obj);
+        assert_eq!(
+            Message::encode(&obj),
+            bytes
+        )
     }
 
     #[test]
-    fn test_decode_write() {}
+    fn test_decode_write() {
+        let bytes = [0, 1, 102, 111, 111, 0, 110, 101, 116, 97, 115, 99, 105, 105, 0];
+        assert_eq!(
+            Message::decode(&bytes).unwrap(),
+            Message::Write { filename: "foo".to_owned(), mode: Mode::NetAscii }
+        );
+    }
 
     #[test]
     fn test_encode_data() {}
