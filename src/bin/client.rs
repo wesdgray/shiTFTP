342bd5a16dc9a92fef7374ef9f2eea1ddc4d5527
Wes Gray <wesgray@gmail.com>
2025-05-03 11:52:50 -0700

    A functional version that has not been tested much

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,6 +2,7 @@ use std::net::UdpSocket;
 use tftp_rs::*;
 
 fn main() {
+<<<<<<< Updated upstream
     let buf = "123456789".as_bytes();
     let socket = UdpSocket::bind("127.0.0.1:42069").unwrap();
     socket.connect("127.0.0.1:69").unwrap();
@@ -11,4 +12,67 @@ fn main() {
     println!("{:?}", enc_read_msg.clone());
     let sent = socket.send(enc_read_msg.as_slice()).unwrap();
     println!("{}", sent);
+=======
+    let cli = Cli::parse();
+    let _ = env_logger::builder()
+        .format_file(true)
+        .format_line_number(true)
+        .try_init();
+
+    match &cli.commands {
+        Commands::Read { filename, server } => {
+            info!("Read the file: {} from {}", filename, server);
+
+            // Network Request to initiate the read
+            let socket = UdpSocket::bind("127.0.0.0:0").unwrap();
+            let read_req = Message::Read {
+                filename: filename.to_string(),
+                mode: Mode::NetAscii,
+            };
+            let recv = socket
+                .send_to(read_req.encode().as_slice(), server)
+                .unwrap();
+            
+            // Setup for writing
+            let mut block_num = 1;
+            let mut buf = [0_u8; 2 + 2 + 512];
+            let file = File::create(filename).unwrap();
+            let mut writer = BufWriter::new(file);
+
+            loop {
+                let (read_bytes, recv_from) = socket.recv_from(&mut buf).unwrap();
+                debug!("Transfer socket is: {recv_from}");
+                debug!("Bytes read: {}", read_bytes);
+
+                if let Ok(Message::Data {
+                    block_num: ack_block_num,
+                    data,
+                }) = buf.as_slice().try_into()
+                {
+                    debug!("Received data for block_num: {block_num}");
+                    if block_num == ack_block_num {
+                        let _ = writer.write(&data).unwrap();
+                        let ack_msg = Message::Ack { block_num };
+                        // TODO: Ensure the ack was received before incrementing block_num
+                        let _ = socket
+                            .send_to(ack_msg.encode().as_slice(), recv_from)
+                            .unwrap();
+                        block_num += 1;
+                    }
+                    
+                    if read_bytes < 512 {
+                        info!("Read less than 512 bytes, transfer complete");
+                        break;
+                    }
+                } else {
+                    debug!("Received bad message: {:?}", buf);
+                }
+            }
+        }
+
+        Commands::Write { filename, server } => {
+            info!("Write the file: {} to {}", filename, server);
+        }
+    }
+>>>>>>> Stashed changes
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -8,10 +8,111 @@ fn main() {
         let bytes_read = socket.recv(&mut buf).unwrap();
         println!("bytes_read: {}", bytes_read);
         let read = buf.to_vec();
+<<<<<<< Updated upstream
         let msg = String::from_utf8(read.clone()).unwrap();
         println!("Vec len: {}", read.len());
         println!("Vec values: {:?}", read);
         println!("{}", msg);
+=======
+        debug!("Vec len {} | values: {:?}", read.len(), read);
+        debug!("As UTF8: {}", String::from_utf8(read).unwrap());
+
+        let copy = buf.clone();
+        if let Ok(message) = copy.as_slice().try_into() {
+            let handle = thread::spawn(move || {
+                handle_transfer(message, sender);
+            });
+            threads.handles.push(handle);
+        } else {
+            warn!("Received invalid message!");
+            debug!("Message content: {:?}", &buf);
+        }
+    }
+}
+
+struct ThreadJoiner {
+    handles: Vec<thread::JoinHandle<()>>,
+}
+
+impl ThreadJoiner {
+    fn new() -> Self {
+        ThreadJoiner { handles: vec![] }
+    }
+}
+
+impl Drop for ThreadJoiner {
+    fn drop(&mut self) {
+        while let Some(handle) = self.handles.pop() {
+            let _ = handle.join();
+        }
+    }
+}
+
+fn handle_transfer(message: Message, remote_addr: SocketAddr) {
+    debug!("Remote Address: {:?}", remote_addr);
+    match message {
+        Message::Read {
+            ref filename,
+            mode: _,
+        } => {
+            info!("Starting Read request for filename: {}", filename);
+            // TODO: Need to disallow .. and absolute paths
+            let file = File::open(filename).unwrap();
+            let mut reader = BufReader::new(file);
+            let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+            socket.connect(remote_addr).unwrap();
+
+            let mut buf = [0_u8; 512];
+            let mut block_num = 1;
+            let mut ack = [0_u8; 4];
+
+            'disk_read: loop {
+                let bytes_read = reader.read(&mut buf).unwrap();
+                let mut data = buf.to_vec();
+                data.truncate(bytes_read);
+
+                let data = Message::Data {
+                    block_num,
+                    data,
+                };
+                debug!("Read {bytes_read} bytes from disk");
+
+                // TODO: some kind of timeout
+                'net_send: loop {
+                    let _ = socket.send(data.encode().as_slice()).unwrap();
+                    debug!("Waiting for Ack on block_num: {block_num}");
+                    let _ = socket.recv(&mut ack);
+                    if let Ok(Message::Ack {
+                        block_num: ack_block_num,
+                    }) = TryInto::<Message>::try_into(&ack as &[u8])
+                    {
+                        if block_num == ack_block_num {
+                            break 'net_send;
+                        }
+                    } else {
+                        debug!("Received invalid message: {:?}", ack);
+                    }
+                }
+
+                if bytes_read < 512 {
+                    info!("Read less than 512 bytes, transfer complete");
+                    break 'disk_read;
+                }
+
+                block_num += 1;
+            }
+        }
+        Message::Write {
+            ref filename,
+            mode: _,
+        } => {
+            info!("Starting Write request for filename: {}", filename);
+        }
+        _ => {
+            // TODO: Send an error back to the client
+            error!("Transfers must be either be a Message::Read or Message::Write!");
+        }
+>>>>>>> Stashed changes
     }
 }
 
